b4a22f006cb539896451766c80c1c41c508e7c0d
Sandro Daudishvili <sdaudishvili@gmail.com>
2022-06-01 20:37:21 +0400

    feat: share and favicon

--- pages/_app.js
+++ pages/_app.js
@@ -4,33 +4,27 @@ import '@/styles/sass/style.scss';
 import 'swiper/swiper.scss';
 
 import PropTypes from 'prop-types';
-import { renderScriptTags } from '@/utils/renderScriptTags';
 
 import { Layout } from '@/components/Layout';
 import { HeaderTagsRenderer } from '@/components/HeaderTagsRenderer';
 
 function App({ Component, pageProps }) {
-  const { analytics = {}, openGraph = {}, socialLinks = {} } = pageProps;
-
-  React.useEffect(() => {
-    renderScriptTags(document.head, analytics.headerCodes);
-    renderScriptTags(document.body, analytics.bodyCodes);
-  }, []);
-
   React.useEffect(() => {
     window.history.scrollRestoration = 'manual';
   }, []);
 
   return (
     <>
-      <HeaderTagsRenderer
-        title={openGraph.title}
-        description={openGraph.description}
-        image={openGraph.image && process.env.STATIC_RESOURCES_URL.concat(openGraph.image)}
-      >
-        <meta property="og:site_name" key="ogSiteName" content={openGraph.title} />
+      <HeaderTagsRenderer title="Dedaena Bar" description="Dedaena Bar" image="/dedaena.png">
+        <meta property="og:site_name" key="ogSiteName" content="Dedaena Bar" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
       </HeaderTagsRenderer>
-      <Layout socialLinks={socialLinks}>
+      <Layout>
         <Component {...pageProps} />
       </Layout>
     </>
